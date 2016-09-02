8d9eb1d4b55a229100923b8665db58da16c016ef
Dongfeng <gudongfeng@outlook.com>
2016-09-02 17:05:02 -0400

    update the ignore file remove the data folder

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
+import copy
+import matplotlib.pyplot as plt
 import tensorflow as tf
-
 from lca_data import LCAData
 from image_processing import inputs
+
 
 FLAGS = tf.app.flags.FLAGS
 
 def main(_):
-  with tf.device('/cpu:0'), tf.Session() as sess:
+  with tf.Graph().as_default(), tf.device('/cpu:0'), tf.Session() as sess:
     # coordinator for controlling queue threads
     coord = tf.train.Coordinator()
-    # writer for tensorboard visualization
-    writer = tf.train.SummaryWriter(
-      "summary",
-      sess.graph)
     # Build an initialization operation to run below.
     init = tf.initialize_all_variables()
     sess.run(init)
     # print out the images
     dataset = LCAData('train')
     images, labels = inputs(dataset)
-    # merge all the summary and then write then out to the writer 
-    summary_op = tf.merge_all_summaries()
+    # merge all the summary and then write then out to the writer
+    input_summaries = tf.get_collection(tf.GraphKeys.SUMMARIES)
+    summary_op = tf.merge_summary(input_summaries)
+    #summary_op = tf.merge_all_summaries()
+    # writer for tensorboard visualization
+    writer = tf.train.SummaryWriter(
+      "summary/train",
+      sess.graph)
     # start all the queue thread
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    # run the ops
+    # run the ops for two iteration
     summary_result, image, label = sess.run([summary_op, images, labels])
     # write the summary result to the writer
-    writer.add_summary(summary_result)
+    writer.add_summary(summary_result, i)
+    # request to stop the input queue
     coord.request_stop()
-
     # Wait for threads to finish.
     coord.join(threads)
 
