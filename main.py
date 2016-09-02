618748112239c3a8e37fe60b92360439a5283dff
Dongfeng <gudongfeng@outlook.com>
2016-09-02 17:23:36 -0400

    finished the image showing function using summary

--- main.py
+++ main.py
@@ -18,19 +18,18 @@ def main(_):
     dataset = LCAData('train')
     images, labels = inputs(dataset)
     # merge all the summary and then write then out to the writer
-    input_summaries = tf.get_collection(tf.GraphKeys.SUMMARIES)
-    summary_op = tf.merge_summary(input_summaries)
-    #summary_op = tf.merge_all_summaries()
+    summary_op = tf.merge_all_summaries()
     # writer for tensorboard visualization
     writer = tf.train.SummaryWriter(
       "summary/train",
-      sess.graph)
+      #graph=sess.graph.as_graph_def(add_shapes=True))
+      graph=sess.graph)
     # start all the queue thread
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     # run the ops for two iteration
     summary_result, image, label = sess.run([summary_op, images, labels])
     # write the summary result to the writer
-    writer.add_summary(summary_result, i)
+    writer.add_summary(summary_result)
     # request to stop the input queue
     coord.request_stop()
     # Wait for threads to finish.
