4c9d3c80c79e96d87b77a74e4c36e3092e06165e
Dongfeng <gudongfeng@outlook.com>
2016-09-02 15:27:30 -0400

    ./gitignore file update

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,41 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Read and preprocess image data.
+
+ Image processing occurs on a single image at a time. Image are read and
+ preprocessed in parallel across multiple threads. The resulting images
+ are concatenated together to form a single batch for training or evaluation.
+
+ -- Provide processed image data for a network:
+ inputs: Construct batches of evaluation examples of images.
+ distorted_inputs: Construct batches of training examples of images.
+ batch_inputs: Construct batches of training or evaluation examples of images.
+
+ -- Data processing:
+ parse_example_proto: Parses an Example proto containing a training example
+   of an image.
+
+ -- Image decoding:
+ decode_jpeg: Decode a JPEG encoded string into a 3-D float32 Tensor.
+
+ -- Image preprocessing:
+ image_preprocessing: Decode and preprocess one image for evaluation or training
+ distort_image: Distort one image for training a network.
+ eval_image: Prepare one image for evaluation.
+ distort_color: Distort the color in one image for training.
+"""
 import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
--- main.py
+++ main.py
@@ -7,32 +7,33 @@ FLAGS = tf.app.flags.FLAGS
 
 def main(_):
   with tf.device('/cpu:0'), tf.Session() as sess:
+    # coordinator for controlling queue threads
     coord = tf.train.Coordinator()
-
+    # writer for tensorboard visualization
+    writer = tf.train.SummaryWriter(
+      "summary",
+      sess.graph)
     # Build an initialization operation to run below.
     init = tf.initialize_all_variables()
     sess.run(init)
-
-    try:
-      # print out the images
-      dataset = LCAData('train')
-      images, labels = inputs(dataset)
-      # merge all the summary and then write then out to the 
-      summary_op = tf.merge_all_summaries()
-      writer = tf.train.SummaryWriter("summary" + '/train',
-                                      sess.graph)
-      # start all the queue thread
-      threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-      # run the ops
-      image, label = sess.run([images, labels])
-      print(image)
-    except Exception as e:
-      print(e)
-    finally:
-      coord.request_stop()
+    # print out the images
+    dataset = LCAData('train')
+    images, labels = inputs(dataset)
+    # merge all the summary and then write then out to the writer 
+    summary_op = tf.merge_all_summaries()
+    # start all the queue thread
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    # run the ops
+    summary_result, image, label = sess.run([summary_op, images, labels])
+    # write the summary result to the writer
+    writer.add_summary(summary_result)
+    coord.request_stop()
 
     # Wait for threads to finish.
     coord.join(threads)
+
+    # close the writer
+    writer.close()
 
 if __name__ == '__main__':
   tf.app.run()
