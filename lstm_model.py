e6fe499a9e25513766587aafd9a39998318f1809
Dongfeng <gudongfeng@outlook.com>
2016-09-12 14:19:57 -0400

    add the birnn function

--- lstm_model.py
+++ lstm_model.py
@@ -33,6 +33,39 @@ def get_config():
   else
     raise ValueError("Invalid model: %s", FLAGS.model)
 
+def BiRNN(x, weights, biases):
+  """Bidrection recurrent neural network"""
+  # Prepare data shape to match `bidirectional_rnn` function requirements
+  # Current data input shape: (n_step, n_row, n_column, n_channel)
+  # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+  # in this case (batch_size, n_input) is equal to (n_row*n_column, n_channel)
+
+  # Reshape to (n_step, n_row*n_column, n_channel), and get the step number
+  x = tf.reshape(x, [-1, 299*299, 3])
+  n_step = tf.shape(x)[0]
+  # Reshape to (n_step*n_row*n_column, n_channel)
+  x = tf.reshape(x, [-1, 3])
+  # Split to get a list of 'n_step' tensors of shape 
+  # (n_row*n_column, n_channel)
+  x = tf.split(0, n_step, x)
+
+  # Define lstm cells with tensorflow
+  # Forward direction cell
+  lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+  # Backward direction cell
+  lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+
+  # Get lstm cell output
+  try:
+    outputs, _, _ = tf.nn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
+                                          dtype=tf.float32)
+  except Exception: # Old TensorFlow version only returns outputs not states
+    outputs = tf.nn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
+                                    dtype=tf.float32)
+
+  # Linear activation, using rnn inner loop last output
+  return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
 def train(dataset):
   # get the configuration settings
   config = get_config()
@@ -50,32 +83,44 @@ def train(dataset):
     'out': tf.Variable(tf.random_normal([config.num_classes]))
   }
 
-def BiRNN(x, weights, biases):
-  """Bidrection recurrent neural network"""
-  # Prepare data shape to match `bidirectional_rnn` function requirements
-  # Current data input shape: (batch_size, n_steps, n_input)
-  # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
-  
-  # Permuting batch_size and n_steps
-  x = tf.transpose(x, [1, 0, 2])
-  # Reshape to (n_steps*batch_size, n_input)
-  x = tf.reshape(x, [-1, n_input])
-  # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-  x = tf.split(0, n_steps, x)
+  pred = BiRNN(x, weights, biases)
 
-  # Define lstm cells with tensorflow
-  # Forward direction cell
-  lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
-  # Backward direction cell
-  lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(n_hidden, forget_bias=1.0)
+  # Define loss and optimizer
+  cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+  optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-  # Get lstm cell output
-  try:
-    outputs, _, _ = tf.nn.rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
-                                          dtype=tf.float32)
-  except Exception: # Old TensorFlow version only returns outputs not states
-    outputs = tf.nn.rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
-                                    dtype=tf.float32)
+  # Evaluate model
+  correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-  # Linear activation, using rnn inner loop last output
-  return tf.matmul(outputs[-1], weights['out']) + biases['out']
+  # Initializing the variables
+  init = tf.initialize_all_variables()
+
+  # Launch the graph
+  with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+      batch_x, batch_y = mnist.train.next_batch(batch_size)
+      # Reshape data to get 28 seq of 28 elements
+      batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+      # Run optimization op (backprop)
+      sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+      if step % display_step == 0:
+        # Calculate batch accuracy
+        acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+        # Calculate batch loss
+        loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+        print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+          "{:.6f}".format(loss) + ", Training Accuracy= " + \
+          "{:.5f}".format(acc))
+      step += 1
+    print("Optimization Finished!")
+
+    # Calculate accuracy for 128 mnist test images
+    test_len = 128
+    test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
+    test_label = mnist.test.labels[:test_len]
+    print("Testing Accuracy:", \
+      sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
